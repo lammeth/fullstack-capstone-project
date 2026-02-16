0eb92b09a8b26a81fe723a0384eee7cf8f3934cf
lammeth <lammeth@gmail.com>
2026-02-16 04:03:27 -0500

    Implement search functionality and style SearchPage

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+//import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
@@ -7,6 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import DetailsPage from './components/DetailsPage/DetailsPage';
+import SearchPage from './components/SearchPage/SearchPage';
 
 function App() {
 
@@ -20,6 +22,7 @@ function App() {
           <Route path="/app/login" element={<LoginPage />} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/product/:productId" element={<DetailsPage />} />
+          <Route path="/app/search" element={<SearchPage />} />
         </Routes>
         </>
   );
